2845b0f463a97bddf8c23fbdac79ba93074d3425
yiqinguo <468894985@qq.com>
2017-08-23 05:41:36 -0400

    add log file inside container

--- filebeat/processor/add_kubernetes_metadata/indexing.go
+++ filebeat/processor/add_kubernetes_metadata/indexing.go
@@ -2,6 +2,7 @@ package add_kubernetes_metadata
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/elastic/beats/libbeat/common"
@@ -21,9 +22,11 @@ func init() {
 }
 
 const LogPathMatcherName = "logs_path"
+const regexMatch = "(.*)-(.{64})\\.log.(.*)"
 
 type LogPathMatcher struct {
 	LogsPath string
+	reg      *regexp.Regexp
 }
 
 func newLogsPathMatcher(cfg common.Config) (add_kubernetes_metadata.Matcher, error) {
@@ -43,7 +46,7 @@ func newLogsPathMatcher(cfg common.Config) (add_kubernetes_metadata.Matcher, err
 		logPath = logPath + "/"
 	}
 
-	return &LogPathMatcher{LogsPath: logPath}, nil
+	return &LogPathMatcher{LogsPath: logPath, reg: regexp.MustCompile(regexMatch)}, nil
 }
 
 func (f *LogPathMatcher) MetadataIndex(event common.MapStr) string {
@@ -54,6 +57,14 @@ func (f *LogPathMatcher) MetadataIndex(event common.MapStr) string {
 		if strings.Contains(source, f.LogsPath) {
 			//Docker container is 64 chars in length
 			cid = source[len(f.LogsPath) : len(f.LogsPath)+64]
+		} else if strings.Contains(source, "/var/log/containers/applogs/") {
+			matcher := f.reg.FindAllStringSubmatch(source, -1)
+			if len(matcher) == 1 {
+				if len(matcher[0]) == 4 {
+					cid = matcher[0][2]
+					event.Put("filepath", matcher[0][3])
+				}
+			}
 		}
 		logp.Debug("kubernetes", "Using container id: ", cid)
 
