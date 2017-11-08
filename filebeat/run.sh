02519d1f41ccfe5b1c1a3f60daa28bac3e00ad8b
yiqinguo <468894985@qq.com>
2017-11-08 15:30:56 +0800

    modify ds file

--- filebeat/run.sh
+++ filebeat/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-df | grep rbd | awk '{print $1}' | xargs umount
+#df | grep rbd | awk '{print $1}' | xargs umount
 hostname=$NODE_NAME
 sed 's/${hostname}/'$hostname'/g' /etc/filebeat/config/beat-template.yml > beat.yml && \
 	./filebeat --c beat.yml --path.data /usr/local/share/filebeat --path.logs /var/log/filebeat
